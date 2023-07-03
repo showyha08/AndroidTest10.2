9a3287afaa3da56a95cc5730aec882073175269a
showyha08 <over.and.uver@gmail.com>
2023-07-03 22:35:43 +0900

    done

--- app/src/main/java/com/websarva/wings/android/databasesample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/databasesample/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private var _cocktailId = -1
     private var _cocktailName = ""
+    private val _helper = DatabaseHelper(this@MainActivity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,9 +24,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 保存ボタンがタップされたときの処理メソッド
-    fun onSaveButtonClick(view: View){
+    fun onSaveButtonClick(view: View) {
         //感想欄を取得
         val etNote = findViewById<EditText>(R.id.etNote)
+
+        //入力された感想を取得
+        val note = etNote.text.toString()
+
+        //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+        val db = _helper.writableDatabase
+
+        // まず、リストで選択されたカクテルのメモデータを削除する
+        // 削除用SQL文字列を用意
+        val sqlDelete = "DELETE FROM cocktailmemos WHERE _id = ?"
+
+        // SQL文字列を元にプリペアドステートメントを取得
+        var stmt = db.compileStatement(sqlDelete)
+
+        // 変数のバインド
+        stmt.bindLong(1, _cocktailId.toLong())
+
+        // 削除SQLの実行
+        stmt.executeUpdateDelete()
+
+        // インサート用SQL文字列の用意
+        val sqlInsert = "INSERT INTO cocktailmemos (_id, name, note) VALUES (?, ?, ?)"
+
+        // SQL文字列を元にプリペアドステートメントを取得
+        stmt = db.compileStatement(sqlInsert)
+
+        // 変数のバインド
+        stmt.bindLong(1, _cocktailId.toLong())
+        stmt.bindString(2, _cocktailName)
+        stmt.bindString(3, note)
+
+        // インサートSQLの実行
+        stmt.executeInsert()
+
         //感想欄の入力値を消去
         etNote.setText("")
         //カクテル名を表示するTextViewを取得
@@ -36,11 +71,12 @@ class MainActivity : AppCompatActivity() {
         val btnSave = findViewById<View>(R.id.btnSave)
         //保存ボタンをタップできないように設定
         btnSave.isEnabled = false
+
     }
 
     // リストがタップされた時の処理が記述されたメンバクラス
     private inner class ListItemClickListner : AdapterView.OnItemClickListener {
-        override fun onItemClick(parent: AdapterView<*>, view:View,position:Int,id:Long) {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
             //タップされた行番号をプロパティの主キーIDに代入
             _cocktailId = position
             //タップされた行のデータを取得。これがカクテル名となるので、プロパティに代入
@@ -52,6 +88,37 @@ class MainActivity : AppCompatActivity() {
             val btnSave = findViewById<View>(R.id.btnSave)
             //保存ボタンをタップできるように設定
             btnSave.isEnabled = true
+
+            //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+            val db = _helper.writableDatabase
+
+            //主キーによる検索カクテルのメモデータを取得するSQL文字列を用意
+            val sql = "SELECT * FROM cocktailmemos WHERE _id = ${_cocktailId}"
+
+            //SQLの実行
+            val cursor = db.rawQuery(sql, null)
+
+            //データベースから取得した値を格納する変数を用意。なかった場合のため初期値は空文字列
+            var note = ""
+
+            //SQL実行の戻り値であるカーソルオブジェクトをループさせてDB内のデータベースを取得
+            while (cursor.moveToNext()) {
+                //カラムのインデックス値を取得
+                val idxNote = cursor.getColumnIndex("note")
+                //カラムのインデックス値を元に実際のデータを取得
+                note = cursor.getString(idxNote)
+            }
+
+            //感想のEditTextの各画面部品を取得しデータベースの値を反映
+            val etNote = findViewById<EditText>(R.id.etNote)
+            etNote.setText(note)
+
         }
+    }
+
+    override fun onDestroy() {
+        // ヘルパーオブジェクトの解放
+        _helper.close()
+        super.onDestroy()
     }
 }
